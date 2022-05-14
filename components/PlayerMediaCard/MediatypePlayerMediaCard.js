fad4d6950adce6cc26d44067ba540efc7dbe4503
jpfraneto <jpfraneto@gmail.com>
2022-05-14 11:57:35 -0400

    Final deploy

--- components/PlayerMediaCard/MediatypePlayerMediaCard.js
+++ components/PlayerMediaCard/MediatypePlayerMediaCard.js
@@ -31,71 +31,9 @@ const MediatypePlayerMediaCard = ({
     rlvRef.current.seekTo(randomPlace, 'seconds');
   };
 
-  const handleShareBtn = () => {
-    const currentTimestamp = Math.floor(rlvRef.current.getCurrentTime());
-    navigator.clipboard.writeText(
-      `${process.env.NEXT_PUBLIC_URL_PATH}/u/${x.author.username}/${x.mediatype}/${x._id}` +
-        `?timestamp=${currentTimestamp}`
-    );
-    setSharedText(true);
-    setTimeout(() => setSharedText(false), 4444);
-  };
-
-  const handleDeleteMedia = async () => {
-    setDeletingMessage('Deleting...');
-    const reqParams = {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        _id: x._id,
-        mediatype: x.mediatype,
-      }),
-    };
-    const deleteResponse = await fetch(
-      `/api/${router.query.username}`,
-      reqParams
-    );
-    const data = await deleteResponse.json();
-    router.push(`/u/${router.query.username}/`);
-  };
   return (
-    <div
-      className={styles.topicContainer}
-      style={{
-        width: bigger ? '60%' : '273px',
-        display: displayedElementId
-          ? mediaForPlaying._id === displayedElementId
-            ? 'inline-block'
-            : 'none'
-          : 'inline-block',
-      }}
-    >
-      <h5>
-        {bigger && (
-          <>
-            {session.user && session.user.username === x.author.username && (
-              <span className={styles.deleteBtn} onClick={handleDeleteMedia}>
-                {deletingMessage ? deletingMessage : 'Delete'}
-              </span>
-            )}
-
-            <span
-              className={styles.closeBtn}
-              onClick={() => {
-                setTimeout(() => setDisplayedElementId(''), 3000);
-                setBigger(false);
-              }}
-            >
-              Close
-            </span>
-          </>
-        )}
-      </h5>
-
-      <div
-        onClick={() => setPlayerWidth('70%')}
-        className={`${styles.playerWrapper} ${styles.gridPlayerWrapper}`}
-      >
+    <div className={styles.topicContainer} style={{}}>
+      <div className={`${styles.playerWrapper} ${styles.gridPlayerWrapper}`}>
         {loadingNext ? (
           <p>L O A D I N G N E X T</p>
         ) : (
@@ -117,33 +55,12 @@ const MediatypePlayerMediaCard = ({
         <div
           className={`${styles.ghostDiv} `}
           onClick={() => {
-            setBigger(true);
-            setDisplayedElementId(mediaForPlaying._id);
+            router.push(
+              `/u/${router.query.username}/${router.query.mediatype}/${mediaForPlaying._id}`
+            );
           }}
-          style={{ display: !bigger ? 'block' : 'none' }}
         ></div>
       </div>
-      {bigger && (
-        <>
-          <div className={styles.descriptionContainer}>
-            <h3>Why is this video here?</h3>
-            {mediaForPlaying.description}
-            <br />
-            <button className={styles.randomBtn} onClick={handleRandomize}>
-              Random Spot
-            </button>
-            <button
-              className={`${styles.randomBtn} ${styles.shareBtn}`}
-              onClick={handleShareBtn}
-            >
-              Share this moment
-            </button>
-          </div>
-          {sharedText && (
-            <p className={styles.copyUrlMessage}>{sharedTextMessage}</p>
-          )}
-        </>
-      )}
     </div>
   );
 };
