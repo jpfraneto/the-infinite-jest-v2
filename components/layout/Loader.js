967285bb5b5d03b796d710059bacb2718b44d125
jpfraneto <jpfraneto@gmail.com>
2022-05-26 13:27:03 -0400

    changed the text for the loader

--- components/layout/Loader.js
+++ components/layout/Loader.js
@@ -8,7 +8,7 @@ const Loader = () => {
       <h2>
         I don&apos;t know how to make the process of loading this page faster.
         I&apos;m sorry for that. But one day I will, or someone will help me,
-        and it will be fuckin awesome.
+        and it will be awesome.
       </h2>
       <div className={styles.loader}></div>
     </div>
