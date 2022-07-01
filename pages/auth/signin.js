73caa0e5b4d4a2d4ae23fc5a247fa2b54f861c66
jpfraneto <jpfraneto@gmail.com>
2022-06-30 23:43:38 -0400

    fixed the stuff

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -2,6 +2,7 @@ import { getProviders, signIn } from 'next-auth/react';
 import styles from '../../styles/Signin.module.css';
 import { FcGoogle } from 'react-icons/fc';
 import { FiGithub } from 'react-icons/fi';
+import Head from 'next/head';
 
 export default function SignIn({ providers }) {
   const getProviderIcon = provider => {
@@ -34,38 +35,43 @@ export default function SignIn({ providers }) {
     welsh: 'Croeso',
   };
   return (
-    <div className={styles.mainContainer}>
-      <div className={styles.loginBox}>
-        <div className={styles.topBox}>
-          {/* <p>
-            Este es un flujo infinito de información en torno a la tecnología
-            que va a revolucionar la forma en la que interactuamos como
-            humanidad:
-          </p>
-          <p className={styles.btcText}>BITCOIN</p> */}
-          <p>The best way in is through.</p>
-        </div>
-        <div className={styles.providerBtnsContainer}>
-          {Object.values(providers).map(provider => (
-            <button
-              key={provider.name}
-              className={styles.signInBtn}
-              onClick={() => signIn(provider.id)}
+    <>
+      <Head>
+        <title>21tv · Entrar</title>
+      </Head>
+      <div className={styles.mainContainer}>
+        <div className={styles.loginBox}>
+          <div className={styles.topBox}>
+            {/* <p>
+          Este es un flujo infinito de información en torno a la tecnología
+          que va a revolucionar la forma en la que interactuamos como
+          humanidad:
+        </p>
+        <p className={styles.btcText}>BITCOIN</p> */}
+            <p>The best way in is through.</p>
+          </div>
+          <div className={styles.providerBtnsContainer}>
+            {Object.values(providers).map(provider => (
+              <button
+                key={provider.name}
+                className={styles.signInBtn}
+                onClick={() => signIn(provider.id)}
+              >
+                {getProviderIcon(provider.name)}
+              </button>
+            ))}
+            <a
+              className={`${styles.contributeBtn}`}
+              target='_blank'
+              rel='noreferrer'
+              href='https://github.com/jpfraneto/the-infinite-jest-v2/blob/main/pages/api/auth/%5B...nextauth%5D.js'
             >
-              {getProviderIcon(provider.name)}
-            </button>
-          ))}
-          <a
-            className={`${styles.contributeBtn}`}
-            target='_blank'
-            rel='noreferrer'
-            href='https://github.com/jpfraneto/the-infinite-jest-v2'
-          >
-            Otro
-          </a>
+              Otro
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
